b70411de0fa34d8eb5c2149a1e4ee026adb699ef
Wamique <wamiqueriyaz00@gmail.com>
2024-05-18 14:05:49 +0530

    added

---
+++ src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+
+export default class Counter extends Component {
+
+    constructor(){
+          super();
+          this.state={
+            value:0
+          }
+          const increment=this.increment.bind(this)
+          const decrement=this.decrement.bind(this)
+    }
+   
+    increment=()=>{
+        this.setState(prev=>({
+           value:prev.value+1 
+        }))
+    }
+    decrement=()=>{
+        this.setState(prev=>({
+            value:prev.value-1
+        }))
+    }
+  render() {
+    return (
+        <>
+        <div>Counter App</div>
+        <p>Counter: {this.state.value}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        </>
+      
+    )
+  }
+}
